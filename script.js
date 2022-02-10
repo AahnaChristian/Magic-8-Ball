2507831f98c2443baa782ef2257ac480847dd346
AahnaChristian <98975884+AahnaChristian@users.noreply.github.com>
2022-02-09 20:57:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,31 +9,65 @@ let ansInputEl = document.getElementById("ansInput");
 magicBallEl.addEventListener("click", responseOut);
 
 // Event Functions
+// function responseOut() {
+//     // Case Insensitive
+//     let queIn = queInputEl.value.toLowerCase();
+
+//     // Set responses to specific questions
+//     if (queIn.innerHTML === "Does a magic 8 ball actually work?" || queIn.innerHTML === "Does a magic 8 ball actually work") {
+//         ansInputEl.innerHTML = "How dare you doubt me!";
+//     } else if (queIn.innerHTML === "Is JavaScript awesome?" || queIn.innerHTML === "Is JavaScript awesome") {
+//         ansInputEl.innerHTML = "Of Course!";
+//     } else if (queIn.innerHTML === undefined) {
+//         ansInputEl.innerHTML = "Please ask a question...";
+//     } else {
+//         // Probability
+//         let randomAns = Math.random();
+    
+//         // Probabilities for the different answers
+//         if (randomAns < 0.2) {
+//             ansInputEl.innerHTML = "Without a Doubt.";
+//         } else if (randomAns < 0.4) {
+//             ansInputEl.innerHTML = "As I see it, yes.";
+//         } else if (randomAns < 0.6) {
+//             ansInputEl.innerHTML = "Concentrate and ask again.";
+//         } else if (randomAns < 0.8) {
+//             ansInputEl.innerHTML = "Don't count on it.";
+//         } else {
+//             ansInputEl.innerHTML = "Outlook not so good.";
+//         }
+//     }
+// }
+
+// Event Functions
 function responseOut() {
     // Case Insensitive
     let queIn = queInputEl.value.toLowerCase();
 
     // Set responses to specific questions
-    if (queIn.innerHTML === "Does a magic 8 ball actually work?" || queIn.innerHTML === "Does a magic 8 ball actually work") {
+    if (queIn === "Does a magic 8 ball actully work?") {
         ansInputEl.innerHTML = "How dare you doubt me!";
-    } else if (queIn.innerHTML === "Is JavaScript awesome?" || queIn.innerHTML === "Is JavaScript awesome") {
+    } else if (queIn === "Is JavaScript awesome?") {
         ansInputEl.innerHTML = "Of Course!";
-    } else if (queIn.innerHTML === undefined) {
-        ansInputEl.innerHTML = "Please ask a question...";
-        
+    } else if (queIn === "Is Visual Studio Code good to use?") {
+        ansInputEl.innerHTML = "Yes, Of Course it is!!";
+    } else {
         // Probability
-        let randomAns = Math.random();
-
-        // Probabilities for the different answers
-        if (randomAns < 0.2) {
-            ansInputEl.innerHTML = "Without a Doubt.";
-        } else if (randomAns < 0.4) {
-            ansInputEl.innerHTML = "As I see it, yes.";
-        } else if (randomAns < 0.6) {
-            ansInputEl.innerHTML = "Concentrate and ask again.";
-        } else if (randomAns < 0.8) {
-            ansInputEl.innerHTML = "Don't count on it.";
-        } else {
-            ansInputEl.innerHTML = "Outlook not so good.";
-        }
+         let randomAns = Math.random();
+    
+         // Probabilities for the different answers
+         if (queIn !== undefined && randomAns < 0.2) {
+             ansInputEl.innerHTML = "Without a Doubt.";
+         } else if (queIn !== undefined && randomAns < 0.4) {
+             ansInputEl.innerHTML = "As I see it, yes.";
+         } else if (queIn !== undefined && randomAns < 0.6) {
+             ansInputEl.innerHTML = "Concentrate and ask again.";
+         } else if (queIn !== undefined && randomAns < 0.8) {
+             ansInputEl.innerHTML = "Don't count on it.";
+         } else if (queIn !== undefined && randomAns < 1) {
+             ansInputEl.innerHTML = "Outlook not so good.";
+         } else if (queIn === undefined){
+            ansInputEl.innerHTML = "Please ask a question...";
+         }
     }
+}
